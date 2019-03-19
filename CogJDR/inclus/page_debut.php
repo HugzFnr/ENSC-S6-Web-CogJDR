88d740c903052be1665420bdbd62434b35f366c4
Grenier Célestin <cgrenier003@ensc.fr>
2019-03-19 11:40:31 +0100

    Du bootstrap
    Implémentation d'un test de navbar dans le `page_debut.php`, ajout des dépendences nécessaires.
    
    (+3h de tentative d'exporter proprement depuis Axure :jpp:)

--- CogJDR/inclus/page_debut.php
+++ CogJDR/inclus/page_debut.php
@@ -2,30 +2,60 @@
 
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <title>CogJDR<?=isset($sous_titre) ? " - $sous_titre" : " !"?></title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <link rel="stylesheet" type="text/css" media="screen" href="./css/discussion.css">
+<head>
+	<meta charset="utf-8">
+	<title>CogJDR<?=isset($sous_titre) ? " - $sous_titre" : " !"?></title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <script src="http://code.jquery.com/jquery-latest.min.js"></script>
-    </head>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+	 crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" media="screen" href="./css/discussion.css">
 
-    <body>
-        <?php
-            if (isset($_SESSION['id'])) {
-                $donnees_jdr = empty($_SESSION['liste_donnees_jdr']) ? "" : $_SESSION['liste_donnees_jdr'][$_SESSION['indice_jdr_suivi']] ?>
-                Utilisateur <?=$_SESSION['email']?> (no<?=$_SESSION['id']?>) <?php
+	<!--script src="http://code.jquery.com/jquery-latest.min.js"></script-->
+</head>
 
-                if (!empty($donnees_jdr)) { ?>
-                    connect&eacute; en tant que <?=$donnees_jdr['nom_joueur'] ?>.<?php 
-                    if (-1 < $donnees_jdr['indice_equipe_discussion_suivi']) { ?>
-                        <br>Affichage de la discussion de l'&eacute;quipe <?=$donnees_jdr['liste_equipe'][$donnees_jdr['indice_equipe_discussion_suivi']]['titre_equipe']?>.<?php
-                    }
-                }
-            }
-        ?>
+<body>
+	<div class="container">
 
-        <h1>Yo!</h1>
-        <hr>
+		<nav class="navbar navbar-expand-lg navbar-light bg-light">
+			<a class="navbar-brand" href="#">Cog' JDR</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+			 aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="navbar-nav mr-auto">
+					<li class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Parties jouées
+						</a>
+						<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+							<a class="dropdown-item" href="#">Partie 0</a>
+							<a class="dropdown-item" href="#">Partie 1</a>
+							<div class="dropdown-divider"></div>
+							<a class="dropdown-item" href="#">Rejoindre une partie</a>
+						</div>
+					</li>
+					<li class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+							Parties masterisées
+						</a>
+						<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+							<a class="dropdown-item" href="#">Partie 0</a>
+							<a class="dropdown-item" href="#">Partie 1</a>
+							<div class="dropdown-divider"></div>
+							<a class="dropdown-item" href="#">Créer une partie</a>
+						</div>
+					</li>
+				</ul>
+				<form class="form-inline my-2 my-lg-0">
+					<input class="form-control mr-sm-2" type="search" placeholder="Code d'invitation" aria-label="Search">
+					<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Rejoindre</button>
+				</form>
+			</div>
+		</nav>
+
+		<h1>Yo!</h1>
+		<hr>
